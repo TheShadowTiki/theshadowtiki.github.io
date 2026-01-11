b1259d048910d872c4a14bc07b36bbea574b3177
TheShadowTiki <mikozekri@gmail.com>
2026-01-11 01:04:51 +0000

    Deploying to gh-pages from @ TheShadowTiki/theshadowtiki.github.io@5e9f0412e958e4cdce35ec27c5312bbf042be473 🚀

--- assets/js/search-data.js
+++ assets/js/search-data.js
@@ -9,14 +9,7 @@ ninja.data = [{
     handler: () => {
       window.location.href = "/";
     },
-  },{id: "nav-research",
-          title: "Research",
-          description: "",
-          section: "Navigation",
-          handler: () => {
-            window.location.href = "/research/";
-          },
-        },{id: "nav-publications",
+  },{id: "nav-publications",
           title: "Publications",
           description: "",
           section: "Navigation",
